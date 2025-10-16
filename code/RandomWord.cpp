9f8a344a5b4ce4ea8365cb8fd3c094d9aa07ffc3
adam-clark-git <adam.b.clark@icloud.com>
2025-10-16 15:56:27 -0600

    Tried to implement randomWord.

--- code/RandomWord.cpp
+++ code/RandomWord.cpp
@@ -1,15 +1,41 @@
+#include "RandomWord.h"
 #include <string>
 #include <iostream>
 #include <fstream> 
-class RandomWord {
-    public:
-        std::string randomize() {
-            std::string newWord;
-            std::fstream myFile("datasets/dataset_words.txt");
+#include <random>
+std::string RandomWord::randomize() {
+    std::string newWord;
+    int wordCount = 0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    
+    std::fstream myFile("datasets/dataset_words.txt");
+    if (!myFile.is_open())
+    {
+        std::cerr << "Unable to open file!" << std::endl;
+        return "";
+    }
 
-            if (!myFile.is_open())
-            {
-                std::cerr << "Unable to open file!" << std::endl;
-            }
+    std::string temp;
+    while (myFile >> temp)
+    {
+        wordCount++;
+    }
+    myFile.clear();  // Clear EOF flag
+    myFile.seekg(0, std::ios::beg);
+
+    std::uniform_int_distribution<> dis(0, wordCount-1);
+    int randomNum = dis(gen);
+
+    int newCount = 0;
+    while (myFile >> newWord)
+    {
+        if (newCount == randomNum)
+        {
+            break;
         }
-};
+        newCount++;
+    }
+    myFile.close();
+    return newWord;
+}
--- code/Word.cpp
+++ code/Word.cpp
@@ -1,11 +1,14 @@
 #include "RandomWord.h"
 #include <string>
+#include <iostream>
+using namespace std;
 class Word {
     std::string realWord;
-    std::string annomyousWord;
+    std::string anonmyousWord;
     public:
         void randomize_word() {
-            
+            RandomWord random;
+            realWord = random.randomize();
         }
         void annomyize_word() {
 
@@ -16,4 +19,11 @@ class Word {
         void reveal_word() {
 
         }
+        
 };
+int main() {
+    Word newWord;
+    newWord.randomize_word();
+    cout << "Hello World!";
+    return 0;
+}
